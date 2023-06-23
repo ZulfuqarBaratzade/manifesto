15eb81ff67b4f37e55cbb98e4f39f5ace80e3e15
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-23 14:50:38 +0400

    Done Logo Settings

--- manifesto/manifesto/core/views.py
+++ manifesto/manifesto/core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from core.models import *
 # Create your views here.
 
+def layout(request):
+    logo = Logo.objects.get(name='logo_image').file
+    context = {
+        'logo':logo,
+    }
+    return context
 def index(request):
     return render(request,'index.html')
