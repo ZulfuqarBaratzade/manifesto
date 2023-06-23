09af7c6bc80ff1cc372344bc27d8ce1284ade32d
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-23 14:38:41 +0400

    create mainbanner models

--- manifesto/manifesto/core/admin.py
+++ manifesto/manifesto/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import *
 
 # Register your models here.
+@admin.register(MainBanner)
+
+class MainBannerAdmin(admin.ModelAdmin):
+    list_display=['id','name','file','updated_date','created_date']
+
+    search_fields = ['id','name']
+    list_editable=['file']
+
+    class Meta:
+        model = MainBanner
--- manifesto/manifesto/core/models.py
+++ manifesto/manifesto/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class AbstractModel(models.Model):
+    updated_date=models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='update_date',
+
+    )
+    created_date=models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='created_date'
+    )
+    class Meta:
+        abstract = True
+
+class MainBanner(AbstractModel):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='name',
+
+    )
+    file = models.ImageField(
+        default='',
+        help_text='',
+        verbose_name='Image',
+        blank=True,
+        upload_to='images/mainbanner',
+    )
+    def __str__(self):
+        return f"Main Banner : {self.name}"
+    class Meta:
+        verbose_name = "Main Banner"
+        verbose_name_plural = "Main Banner"
+        ordering = ('name',)
+    
